1a3bac6ccadd08e3228be462ac629f55a7c699f4
JarysonUERN <jaryson20240025638@alu.uern.br>
2026-07-11 03:50:13 -0300

    fix

--- BackEnd/app/controllers/application_controller.rb
+++ BackEnd/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :unprocessable
   rescue_from ActionController::ParameterMissing, with: :bad_request
